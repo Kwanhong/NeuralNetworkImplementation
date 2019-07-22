bd429bb7f013f4324b3ab4e4da609ff3292cb5a1
TRheed <rhksghd8303@naver.com>
2019-07-22 17:51:57 +0900

    Initial Commit

---
+++ Scripts/System/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neuralnetworkImplementation
+{
+    static class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
